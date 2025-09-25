2a1b6a55d122754b241835e1dc80f15f0c15878e
GisethMurcia <murciagiseth@gmail.com>
2025-09-25 16:15:06 -0500

    Menu Biblioteca: Seleccion de Modulos

---
+++ biblioteca/src/main/java/com/ModuloLibros.java
@@ -0,0 +1,10 @@
+package com;
+
+public class ModuloLibros {
+
+    public ModuloLibros() {
+    System.out.println(" \n --- Módulo de Gestión de Libros --- ");
+        // Aquí puedes agregar la lógica para gestionar libros
+    }
+    
+}
---
+++ biblioteca/src/main/java/com/ModuloPrestamos.java
@@ -0,0 +1,10 @@
+package com;
+
+public class ModuloPrestamos {
+    
+    public ModuloPrestamos() {
+        System.out.println(" \n --- Módulo de Gestión de Préstamos --- ");
+        // Aquí puedes agregar la lógica para gestionar préstamos
+    }
+    
+}
